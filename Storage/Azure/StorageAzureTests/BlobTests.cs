1116590f30a95a0b0e5028826ce338729576ca98
Juan Quijano <juan.c.quijano.abad@avanade.com>
2016-07-07 20:14:18 +0200

    Añadiendo material de pruebas
    Related Work Items: #44

---
+++ Storage/Azure/StorageAzureTests/BlobTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorageAzure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAzure.Tests
+{
+    [TestClass()]
+    public class BlobTests
+    {
+        [TestMethod()]
+        public void PutTest()
+        {
+            var Blob = new Blob();
+            var objeto = Stream.Null;
+
+            var resultado = Blob.Put(objeto);
+
+            Assert.IsTrue(resultado);
+        }
+    }
+}
